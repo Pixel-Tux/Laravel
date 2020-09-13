9169271eba00f638507cf8451cf906b1e66fb482
Luis Pardus <luis.pardus@gmail.com>
2020-09-13 22:09:30 +0200

    index

---
+++ resources/views/articles/index.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('content')
+<div id="wrapper">
+	<div id="page" class="container">
+        @foreach ($articles as $article)
+            <div id="content">
+                <div class="title">
+                    <a href="/articles/{{ $article->id }}">
+                        <h2>{{ $article->title }}</h2>
+                    </a>
+                </div>
+
+                
+                <p>
+                    <img src="/images/banner.jpg" alt="" class="image image-full"> 
+                </p>
+                {{$article->body}}
+            </div>
+        @endforeach
+	</div>
+</div>   
+@endsection
--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div id="wrapper">
 	<div id="page" class="container">
-		<div id="content">
+		<div class="content">
 			<div class="title">
                 <h2>{{ $article->title }}</h2>
             </div>
