42038eeae4377d3fd38b2fcf0b8cd9264d68619a
Luis Pardus <luis.pardus@gmail.com>
2020-09-13 13:38:15 +0200

    mysql requeste

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -6,17 +6,21 @@ use Illuminate\Http\Request;
 
 class PostsController extends Controller
 {
-    public function show($post){
-        $posts = [
-                    'my-first-post' => 'Hello, this is my first blog post!',
-                    'my-second-post' => 'Now I am getting the hand of this blogging thing'
-                ];
-                if (!array_key_exists($post, $posts)){
-                    abort (404, 'Sorry, that post was not found.');
-                }
+    public function show($slug){
+
+        $post = \DB::table('posts')->where('slug', $slug)->first();
+
+        //dd($post);
+        // $posts = [
+        //             'my-first-post' => 'Hello, this is my first blog post!',
+        //             'my-second-post' => 'Now I am getting the hand of this blogging thing'
+        //         ];
+                // if (!array_key_exists($post, $posts)){
+                //     abort (404, 'Sorry, that post was not found.');
+                // }
             
                 return view('posts', [
-                    'post' => $posts[$post] 
+                    'post' => $post 
                     // ?? 'Nothing here yet'
                 ]);
     }
