723d9cd2955094f1b81e6b9631d20415eb8725c4
Luis Pardus <luis.pardus@gmail.com>
2020-09-13 14:27:38 +0200

    eloquent pull db

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,27 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use App\Post;
 
 class PostsController extends Controller
 {
     public function show($slug){
-
-        $post = \DB::table('posts')->where('slug', $slug)->first();
-        if (!$post){
-            abort(404);
-        }
+        
         //dd($post);
         // $posts = [
         //             'my-first-post' => 'Hello, this is my first blog post!',
         //             'my-second-post' => 'Now I am getting the hand of this blogging thing'
         //         ];
-                // if (!array_key_exists($post, $posts)){
-                //     abort (404, 'Sorry, that post was not found.');
-                // }
-            
-                return view('posts', [
-                    'post' => $post 
-                    // ?? 'Nothing here yet'
-                ]);
+
+        // if (!array_key_exists($post, $posts)){
+        //     abort (404, 'Sorry, that post was not found.');
+        // }
+
+        //$post = DB::table('posts')->where('slug', $slug)->first();
+        // if (!$post){
+        //     abort(404);
+        // }
+
+
+        
+        
+        return view('posts', [
+            'post' => Post::where('slug', $slug)->firstOrFail()
+            // 'post' => $post 
+        ]);
     }
 }
