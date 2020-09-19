49564c6f9a6aa58266bfc684ec1237ba54a348f5
Luis Pardus <luis.pardus@gmail.com>
2020-09-19 16:24:44 +0200

    basic eloquent

--- app/User.php
+++ app/User.php
@@ -36,4 +36,15 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function articles(){
+        return $this->hasMany(Article::class);
+    }
+    public function projects(){
+        return $this->hasMany(Project::class);
+    }
+
+    // $user = User::finde(1); select * from user where id = 1;
+    // $user->projects; select * from projects  where user_id = $user->id
+    // $user->projects-> 
 }
