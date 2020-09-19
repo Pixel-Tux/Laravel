dbcdac2fe8744198bfd820c077745598daaed863
Luis Pardus <luis.pardus@gmail.com>
2020-09-19 09:40:16 +0200

    restful controller

--- app/Http/Controllers/ArticlesController.php
+++ app/Http/Controllers/ArticlesController.php
@@ -7,15 +7,42 @@ use Illuminate\Http\Request;
 
 class ArticlesController extends Controller
 {
-    public function index()
-    {
+    // render a list of resurces.
+    public function index() {
         $articles = Article::latest()->get();
         return view('articles.index', ['articles' => $articles]);
     }
 
-    public function show($id)
-    {
+    // shows a single resorce
+    public function show($id) {
         $article = Article::find($id);
         return view('articles.show', ['article' => $article]);
     }
+
+    // shows a view to create a new resorce
+    public function create() {
+
+    }
+
+    // persist the new resorce
+    public function store() {
+
+    }
+    
+    // show a wiew to edit an existing resource
+    public function edit() {
+
+    }
+
+    // persist the edited rdsorce
+    public function update() {
+
+    }
+
+    // delete the risisorce
+    public function destroy() {
+
+    }
+
+
 }
